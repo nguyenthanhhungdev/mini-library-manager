be037f1781e8abf4f32cf46ab81d21b25c25155c
test <nguyenthanhhung.dev@gmail.com>
2022-12-01 02:34:00 +0700

    Update Documents.edit

--- quanlythuvien/src/main/java/ThuVien/Documents.java
+++ quanlythuvien/src/main/java/ThuVien/Documents.java
@@ -1,8 +1,10 @@
 package ThuVien;
 
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import Polyfill.PFArray;
 import Polyfill.StringHelper;
@@ -143,11 +145,32 @@ public class Documents extends Management<Document> {
                     System.out.println("Dang thao tac edit tai lieu: ");
                     System.out.println(document.toString());
                     System.out.println("Chon thao tac: ");
-                    switch (m = StringHelper.acceptInput("Ten", "Ngay xuat ban", "So luong ban sao")) {
-                        case 1 -> document.setName(StringHelper.acceptLine("Nhap ten gia: "));
-                        case 2 ->
-                                document.setPublication(ThoiGian.parseTG(StringHelper.acceptLine("Nhap ngay xuat ban: ")));
+                    switch (m = StringHelper.acceptInput("Ten", "Tac gia", "Ngay xuat ban", "So luong ban sao")) {
+                        case 1 -> document.setName(StringHelper.acceptLine("Nhap ten tai lieu: "));
+                        case 2 -> {
+                            Stream.of(document.getAuthors()).map(i -> i.toString()).forEach(i -> System.out.println(i));
+
+                            int id_Bechanged = Integer.parseInt(StringHelper.acceptLine("Nhap ma tac gia muon edit: "));
+
+                            Document finalDocument = document;
+                            //Lưu lại vị trí author cần thay đổi trong
+                            int index = IntStream.range(0, document.getAuthors().length).filter(i -> finalDocument.getAuthors()[i].getId() == id_Bechanged).findAny().orElse(-1);
+
+                            Author[] new_authors = new Author[0];//Mảng mới lưu lại các giá trị
+                            if (index != -1) {
+                                int id_changed = Integer.parseInt(StringHelper.acceptLine("Nhap ma tac gia moi: "));
+                                int check = Global.authors.search(id_changed);//Lưu lại vị trí author mới trong Global
+
+                                if (check != -1) {
+                                    new_authors = Arrays.copyOf(document.getAuthors(), document.getAuthors().length);
+                                    new_authors[index] = Global.authors.instance.at(check);//Thay đổi author
+                                }
+                            }
+                            document.setAuthors(new_authors);
+                        }
                         case 3 ->
+                                document.setPublication(ThoiGian.parseTG(StringHelper.acceptLine("Nhap ngay xuat ban: ")));
+                        case 4 ->
                                 document.setCopies(Integer.parseInt(StringHelper.acceptLine("Nhap so luong ban sao: ")));
                         default -> {
                             m = -1;
